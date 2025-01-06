9232c08e8d5eb1cb35c8b6ed9d9393dbcf8d4802
Aditya Kumar Mishra <154746713+adityakrmishra@users.noreply.github.com>
2025-01-06 20:04:50 +0530

    Create AddTodo.js

---
+++ src/components/AddTodo.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const AddTodo = ({ addTodo }) => {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo({
+      text: value,
+      isCompleted: false,
+    });
+    setValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default AddTodo;
